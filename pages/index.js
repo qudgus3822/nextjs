8dc56441c8a77bb1746586a0404c30d22437fb61
bhkim <bhkim@stis.co.kr>
2022-10-17 17:01:34 +0900

    .

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World. 아임 병현 <Link href="/about">About</Link>
+      Hello World. 아임 병현12 <Link href="/about">About</Link>
     </div>
   )
 }
